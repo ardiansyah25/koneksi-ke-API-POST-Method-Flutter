ccb2ca0fdd9a3f443a34a6896202d673abf665fb
Ardiansyah <ardidiskominfokampar@gmail.com>
2021-08-18 15:13:47 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/konek_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.konek_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:konek_api/post_result_model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PostResult postResult;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("API Demo"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text((postResult != null)
+                  ? postResult.id + "|" + postResult.name
+                  : "tidak ada"),
+              ElevatedButton(
+                  onPressed: () {
+                    PostResult.connectToAPI("baju", "dokter").then((value) {
+                      postResult = value;
+                      setState(() {});
+                    });
+                  },
+                  child: Text("POST")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
